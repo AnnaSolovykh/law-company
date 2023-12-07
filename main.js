742bd4fb89ba0e013946aa88db04c0fffcf80a83
AnnaSolovykh <ms.teachy.solo@gmail.com>
2023-12-07 12:10:54 +0300

    add fixed on scroll  contact button

--- main.js
+++ main.js
@@ -81,8 +81,24 @@ document.addEventListener('DOMContentLoaded', function() {
     updateSlider();
 });
 
+window.onscroll = function() {
+    scrollFunction()
+};
+
+function scrollFunction() {
+    const contactsContainer = document.querySelector("#contactActions");
+    const toTopButton = document.querySelector("#toTopButton");
+    if (document.body.scrollTop > 150 || document.documentElement.scrollTop > 150) {
+        contactsContainer.style.display = "block";
+        toTopButton.style.display = "block";
+    } else {
+        contactsContainer.style.display = "none";
+        toTopButton.style.display = "none";
+    }
+}
 
 function topFunction() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 }
+
