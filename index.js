736e4245ebf753365c6f2dd905c5210b9c129788
AnnaSolovykh <ms.teachy.solo@gmail.com>
2024-01-08 14:11:17 +0300

    refactor js files

--- index.js
+++ index.js
@@ -1,35 +1,3 @@
-//Navbar
-const burgerIcon = document.querySelector('.burger-icon');
-const navbarMenu =  document.querySelector('.navbar-menu');
-const menuItems = navbarMenu.querySelectorAll('a'); 
-
-burgerIcon.addEventListener('click', () => {
-    burgerIcon.classList.toggle('toggle');
-    if (navbarMenu.classList.contains('active')) {
-        navbarMenu.classList.remove('active');
-        setTimeout(() => {
-            navbarMenu.style.display = 'none';
-        }, 500); 
-    } else {
-        navbarMenu.style.display = 'flex';
-        requestAnimationFrame(() => {
-            navbarMenu.classList.add('active');
-        });
-    }
-});
-
-menuItems.forEach(item => {
-    item.addEventListener('click', () => {
-        if (navbarMenu.classList.contains('active')) {
-            navbarMenu.classList.remove('active');
-            setTimeout(() => {
-                navbarMenu.style.display = 'none';
-            }, 500); 
-            burgerIcon.classList.toggle('toggle');
-        }
-    });
-});
-
 //Language Switcher
 document.addEventListener('DOMContentLoaded', function() {
     const enFlag = document.getElementById('enFlag');
