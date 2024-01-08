736398fc0d16123c5113062c8d8a66643337adb9
AnnaSolovykh <ms.teachy.solo@gmail.com>
2024-01-08 16:21:46 +0300

    refactor js files

--- index.js
+++ index.js
@@ -1,27 +1,3 @@
-//Language Switcher
-document.addEventListener('DOMContentLoaded', function() {
-    const enFlag = document.getElementById('enFlag');
-    const ruFlag = document.getElementById('ruFlag');
-    
-    const currentLanguage = window.location.pathname.includes('/ru') ? 'ru' : 'en';
-    
-    if (currentLanguage === 'en') {
-        ruFlag.style.display = 'block';
-        enFlag.style.display = 'none';
-    } else {
-        enFlag.style.display = 'block';
-        ruFlag.style.display = 'none';
-    }
-
-    enFlag.addEventListener('click', function() {
-        window.location.href = window.location.pathname.replace(/^\/ru/, '/en');
-    });
-
-    ruFlag.addEventListener('click', function() {
-        window.location.href = window.location.pathname.replace(/^\/en/, '/ru');
-    });
-});
-
 //Slider
 document.addEventListener('DOMContentLoaded', function() {
     const slider = document.querySelector('.slider');
--- main.js
+++ main.js
@@ -30,6 +30,30 @@ menuItems.forEach(item => {
     });
 });
 
+//Language Switcher
+document.addEventListener('DOMContentLoaded', function() {
+    const enFlag = document.getElementById('enFlag');
+    const ruFlag = document.getElementById('ruFlag');
+    
+    const currentLanguage = window.location.pathname.includes('/ru') ? 'ru' : 'en';
+    
+    if (currentLanguage === 'en') {
+        ruFlag.style.display = 'block';
+        enFlag.style.display = 'none';
+    } else {
+        enFlag.style.display = 'block';
+        ruFlag.style.display = 'none';
+    }
+
+    enFlag.addEventListener('click', function() {
+        window.location.href = window.location.pathname.replace(/^\/ru/, '/en');
+    });
+
+    ruFlag.addEventListener('click', function() {
+        window.location.href = window.location.pathname.replace(/^\/en/, '/ru');
+    });
+});
+
 //Footer
 window.onscroll = function() {
     scrollFunction()
