1e893b928b01c7d0662096f332d700c8afedc7d7
AnnaSolovykh <ms.teachy.solo@gmail.com>
2024-01-16 12:56:27 +0300

    add swiper bg wrapper and improve breakpoints

--- index.js
+++ index.js
@@ -35,25 +35,61 @@ const swiper = new Swiper(".swiper-slider", {
         slidesPerView: 1.5,
         spaceBetween: 20
       },
-      600: {
+      560: {
+        slidesPerView: 1.75,
+        spaceBetween: 20
+      },
+      650: {
         slidesPerView: 2,
-        spaceBetween: 20
+        spaceBetween: 10
       },
-      1024: {
+      710: {
+        slidesPerView: 2.25,
+        spaceBetween: 10
+      },
+      820: {
+        slidesPerView: 2.35,
+        spaceBetween: 10
+      },
+      880: {
         slidesPerView: 2.5,
-        spaceBetween: 20
+        spaceBetween: 10
       },
-      1200: {
+      950: {
+        slidesPerView: 2.75,
+        spaceBetween: 10
+      },
+      1010: {
+        slidesPerView: 2.9,
+        spaceBetween: 15
+      },
+      1090: {
         slidesPerView: 3,
-        spaceBetween: 20
+        spaceBetween: 37
       },
-      1350: {
-        slidesPerView: 3.5,
-        spaceBetween: 20
+      1211: {
+        slidesPerView: 3.25,
+        spaceBetween: 40
+      },
+      1300: {
+        slidesPerView: 3.75,
+        spaceBetween: 10
+      },
+      1390: {
+        slidesPerView: 4,
+        spaceBetween: 10
       },
       1500: {
-        slidesPerView: 4,
-        spaceBetween: 20
-      }
+        slidesPerView: 4.25,
+        spaceBetween: 10
+      },
+      1610: {
+        slidesPerView: 4.6,
+        spaceBetween: 10
+      },
+      1800: {
+        slidesPerView: 5,
+        spaceBetween: 10
+      },
     }
   });
